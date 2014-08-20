59e27c60f0f1678b362bee20286f7aff41eea4fb
Simon <liutayu@hotmail.com>
2014-08-20 12:05:57 +0800

    fb access token in session

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,6 +9,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication # This will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+
+      session[:fb_access_token] = request.env['omniauth.auth']['credentials']['token']
     else
       #session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
